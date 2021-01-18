b46edba080e51eff7d59ab8c0120087017d3b997
Xitaymin <y.barashenko@gmail.com>
2021-01-18 22:58:06 +0200

    Added regex for comment, phone, email

--- src/ua/training/ControllerTest.java
+++ src/ua/training/ControllerTest.java
@@ -1,40 +1,80 @@
 package ua.training;
 
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.*;
-
 public class ControllerTest extends Controller {
     private String[] validSurnameExamples;
     private String[] invalidSurnameExamples;
+    private String[] validCommentsExamples;
+    private String[] validPhoneNumbers;
+    private String[] validEmailExamples;
+    private String[] invalidEmailExamples;
 
     @Before
-    public void prepareValidData() throws Exception {
-        validSurnameExamples = new String[]{"Єненков","Нечуй-Левицький","О'Коннор","Smith"};
+    public void prepareValidSurnames() throws Exception {
+        validSurnameExamples = new String[]{"Єненков", "Нечуй-Левицький", "О'Коннор", "Smith"};
     }
 
     @Test
     public void testIfValidSurnameAccepted() {
-        for (String s : validSurnameExamples) {
-            Assert.assertTrue(checkIfUserInputValid(RegexContainer.SURNAME, s));
-        }
+        checkIfEveryValidItemAccepted(validSurnameExamples, RegexContainer.SURNAME);
     }
 
     @Before
-    public void prepareInvalidData() {
-        invalidSurnameExamples = new String[]{"Ьсин","","A","B0nd"};
+    public void prepareInvalidSurnames() {
+        invalidSurnameExamples = new String[]{"Ьсин", "", "A", "B0nd"};
     }
 
     @Test
     public void testIfInvalidSurnameRejected() {
-        for (String s : invalidSurnameExamples) {
-            Assert.assertFalse(checkIfUserInputValid(RegexContainer.SURNAME, s));
+        checkIfEveryInvalidItemRejected(invalidSurnameExamples, RegexContainer.SURNAME);
+    }
+
+    @Before
+    public void prepareValidComment() {
+        validCommentsExamples = new String[]{"Comment12", "Комментарий"};
+    }
+
+    @Test
+    public void testIfValidCommentAccepted() {
+    checkIfEveryValidItemAccepted(validCommentsExamples,RegexContainer.COMMENT);
+    }
+
+    @Before
+    public void prepareValidPhoneNumbers() {
+        validPhoneNumbers = new String[]{"+380965841732"};
+    }
+
+    @Test
+    public void testIfValidPhoneNumberAccepted(){
+        checkIfEveryValidItemAccepted(validPhoneNumbers,RegexContainer.PHONE);
+    }
+
+    @Before
+    public void prepareValidEmails(){
+        validEmailExamples = new String[]{"ivanov@inbox.ru"};
+    }
+
+    @Test
+    public void testIfValidEmailAccepted(){
+        checkIfEveryValidItemAccepted(validEmailExamples,RegexContainer.EMAIL);
+    }
+
+    public void checkIfEveryValidItemAccepted(String[] validData, String regex) {
+        for (String item : validData) {
+            Assert.assertTrue(checkIfUserInputValid(regex, item));
+            System.out.println(item);
         }
     }
+
+    public void checkIfEveryInvalidItemRejected(String[] inValidData, String regex) {
+        for (String item : inValidData) {
+            Assert.assertFalse(checkIfUserInputValid(regex, item));
+            System.out.println(item);
+        }
+    }
+
+
 }
--- src/ua/training/RegexContainer.java
+++ src/ua/training/RegexContainer.java
@@ -1,12 +1,11 @@
 package ua.training;
 
 public interface RegexContainer {
-    /*
-     * Shouldn't contain digits, may include few words, symbols -,' every word should contain less than 17 symbols and begin from capital letter
-     */
-    final String SURNAME = "[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}";
-    final String SURNAME_ADVANCED = "[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}";
-    final String NAME = "[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}";
-    final String PATRONYMIC = "[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}";
-    final String NICKNAME = "[A-Za-z0-9]{6,20}";
+    final String SURNAME = "^[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}$";
+    final String NAME = "^[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}$";
+    final String PATRONYMIC = "^[A-ZА-ЩЭ-ЯІЇЄа-яіїa-z'-]{2,20}$";
+    final String NICKNAME = "^[A-Za-z0-9_]{6,20}$";
+    final String COMMENT = ".{0,60}";
+    final String PHONE = "^\\+380\\d{9}$";
+    final String EMAIL = "^\\w{1,64}@[\\w-\\.]{1,253}\\.[a-z]{2,3}$";
 }
