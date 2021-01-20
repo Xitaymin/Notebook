4cecb2dafa677c6f616668a69c1b12bec2ea6c51
Xitaymin <y.barashenko@gmail.com>
2021-01-20 23:00:12 +0200

    Added table for storage fieldname - regex pairs

--- src/ua/training/Controller.java
+++ src/ua/training/Controller.java
@@ -1,15 +1,36 @@
 package ua.training;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Controller {
     private Scanner scanner;
+    private Map<String, String> noteFields;
 
     public void initial() {
+        noteFields = new LinkedHashMap<>();
+        noteFields.put("surname",RegexContainer.SURNAME);
+        noteFields.put("name",RegexContainer.NAME);
+        noteFields.put("patronymic",RegexContainer.PATRONYMIC);
+        noteFields.put("nick",RegexContainer.NICKNAME);
+        noteFields.put("comment",RegexContainer.COMMENT);
+        noteFields.put("homePhone",RegexContainer.PHONE);
+        noteFields.put("cellPhone",RegexContainer.PHONE);
+        noteFields.put("cellPhone2",RegexContainer.PHONE);
+        noteFields.put("email",RegexContainer.EMAIL);
+        noteFields.put("skype",RegexContainer.SKYPE);
+
         Scanner scanner = new Scanner(System.in);
     }
+
+
+    public void processUserInput() {
+
+    }
+
     public String getUserInput(Scanner scanner) {
         return scanner.nextLine();
     }
