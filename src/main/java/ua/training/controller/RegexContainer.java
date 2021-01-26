00f469eeaf6249c281b93345a5e769ba473a0915
Xitaymin <y.barashenko@gmail.com>
2021-01-26 21:50:40 +0200

    Added input request messages for ua locale

--- src/main/java/ua/training/controller/RegexContainer.java
+++ src/main/java/ua/training/controller/RegexContainer.java
@@ -9,7 +9,7 @@ public interface RegexContainer {
      String PHONE = "^\\+380\\d{9}$";
      String CELL_PHONE2 = "^(\\+380\\d{9}|\\s*)$";
      String EMAIL = "^[^.\\W][-\\w._]{5,63}@[\\w-.]{1,253}\\.[a-z]{2,3}$";
-     String EMAIL_ADVANCED = "^[^.\\W][\\w]{5,30}@[\\w-\\.]{1,253}\\.[a-z]{2,3}$";
+//     String EMAIL_ADVANCED = "^[^.\\W][\\w]{5,30}@[\\w-\\.]{1,253}\\.[a-z]{2,3}$";
      String SKYPE = "^[-A-z0-9._]{6,20}$";
      String INDEX = "\\d{5}";
      String CITY = "^[^а-яЭЫЪ][А-ЩЬЮЯа-щьюяІЇЄіїє'\\s-]{1,20}$";
